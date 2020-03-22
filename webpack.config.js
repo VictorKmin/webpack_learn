8f1323a75e2af7a82229029d906c62a4c12546f4
VictorKmin <kminvictor@gmail.com>
2020-03-22 11:40:34 +0200

    add new features

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,33 @@ const path = require('path');
 
 const HTMLWebPackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebPuckPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+// [name] береться з entry по ключах, [hash] -> hash файлу
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const optimization = () => {
+  const config = {
+    // Виносить дубльований код в окремі файли
+    splitChunks: {
+      chunks: "all"
+    }
+  };
+
+  if (!isDev) {
+    // Мініфікує HTML та CSS
+    config.minimizer = [
+      new OptimizeCSSAssetsPlugin(),
+      new TerserWebPuckPlugin
+    ]
+  }
+  return config;
+};
 
 module.exports = {
   // Де лежать сорс коди. При вказування шляху до файлів ./src можна не писати
@@ -15,17 +42,11 @@ module.exports = {
   },
   output: {
     // файли які будуть включені в білд.
-    // [name] береться з entry по ключах, [contenthash] -> hash контену. Міняється при зміні констексту файлу
-    filename: "[name].[contenthash].js",
+    filename: filename('js'),
     // Куди білдити
     path: path.resolve(__dirname, 'dist')
   },
-  optimization: {
-    // Виносить дубльований код в окремі файли
-    splitChunks: {
-      chunks: "all"
-    }
-  },
+  optimization: optimization(),
   devServer: {
     // Зробить сервер який буде автоматом підтягувтаи код. Аналог англуяра. Тримає весь код в опертивці
     // Для роботи треба прописати в package.json скрпт який використовує dev-server
@@ -40,17 +61,55 @@ module.exports = {
   plugins: [
     new HTMLWebPackPlugin({
       // Вказує на файл який буде братись в якості точки входу HTML. Копіює його з src в dist
-      template: "./index.html"
+      template: "./index.html",
+      minify: {
+        // Стрирає всі лишні пробіли в html
+        collapseWhitespace: !isDev
+      }
     }),
     // Агалог rimraf для webpack
-    new CleanWebpackPlugin({})
+    new CleanWebpackPlugin({}),
+    // Плагін для копіювання файлів або папок. Переносить з однієї папки в іншу.
+    // Можна використовувати на файли або папки
+    new CopyWebPackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCSSExtractPlugin({
+      filename: filename('css'),
+    })
   ],
   module: {
     rules: [
       {
-        // Коли webpack бачить .css, то використовує правила з use
+        // Якщо файли закінчуються на .css то я виконристовую такі правила.
         test: '/\.css$/',
-        use: ['style-loader','css-loader'] // порядок важливий.
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: {
+              hmr: isDev, // Hot Module Replacement. Перезавантажує стилі після зміни
+              reloadAll: true
+            }
+          },
+          'css-loader'
+        ] // порядок важливий.
+      },
+      {
+        test: '/\.sass/',
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: {
+              hmr: isDev, // Hot Module Replacement. Перезавантажує стилі після зміни
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ] // порядок важливий.
       },
       {
         test: '/\.(png|jpg|jpeg|svg|gif)$/',
